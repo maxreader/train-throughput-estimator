79b00c8429bfc2e456f7f7db3b486378e80fee21
60635199 <60635199+maxreader@users.noreply.github.com>
2021-06-13 14:28:49 -0500

    Lots of shit, almost fully functional

--- data.lua
+++ data.lua
@@ -30,7 +30,42 @@ data:extend{
         flags = {"hidden", "only-in-cursor", "not-stackable"},
         draw_label_for_cursor_render = true,
         entity_type_filters = rollingStockTypes
+    }, {
+        type = "sprite",
+        name = "tte-up-arrow",
+        filename = "__core__/graphics/arrows/table-header-sort-arrow-up-white.png",
+        priority = "extra-high",
+        size = 16,
+        flags = {"gui-icon"}
+    }, {
+        type = "sprite",
+        name = "tte-up-arrow-hover",
+        filename = "__core__/graphics/arrows/table-header-sort-arrow-up-hover.png",
+        priority = "extra-high",
+        size = 16,
+        flags = {"gui-icon"}
+    }, {
+        type = "sprite",
+        name = "tte-down-arrow",
+        filename = "__core__/graphics/arrows/table-header-sort-arrow-down-white.png",
+        priority = "extra-high",
+        size = 16,
+        flags = {"gui-icon"}
+    }, {
+        type = "sprite",
+        name = "tte-down-arrow-hover",
+        filename = "__core__/graphics/arrows/table-header-sort-arrow-down-hover.png",
+        priority = "extra-high",
+        size = 16,
+        flags = {"gui-icon"}
     }
+}
+
+styles.tte_fuel_choose_elem_button = {
+    type = "button_style",
+    parent = "slot_button",
+    height = 30,
+    width = 30
 }
 
 styles.tte_toolbar_frame = {
@@ -62,9 +97,16 @@ styles.tte_rates_list_box_row_frame = {
 styles.tte_data_frame = {
     type = "frame_style",
     parent = "subfooter_frame",
-    top_padding = 4,
-    left_padding = 12,
-    right_padding = 12,
+    top_padding = 2,
+    bottom_padding = 2,
+    height = 36,
+    horizontally_stretchable = "on"
+}
+
+styles.tte_junction_size_frame = {
+    type = "frame_style",
+    parent = "subfooter_frame",
+    top_padding = 2,
     bottom_padding = 2,
     height = 36
 }
@@ -102,13 +144,35 @@ styles.tte_rates_list_box_scroll_pane = {
     vertical_flow_style = {type = "vertical_flow_style", vertical_spacing = 0}
 }
 
--- TODO:
---[[
-  tool.selection_color = data.color
-  tool.selection_cursor_box_type = data.selection_box
-  local alt_color = table.shallow_copy(data.color)
-  -- temporary?
-  alt_color.b = 0.7
-  tool.alt_selection_color = alt_color
-  tool.alt_selection_cursor_box_type = data.selection_box
-  selection_tools[#selection_tools+1] = tool]]
+styles.tte_consist_list_box_scroll_pane = {
+    type = "scroll_pane_style",
+    extra_padding_when_activated = 0,
+    padding = 0,
+    horizontally_stretchable = "on",
+    -- height is defined by a per-player setting
+    graphical_set = {shadow = default_inner_shadow},
+    background_graphical_set = {
+        position = {282, 17},
+        corner_size = 8,
+        overall_tiling_horizontal_padding = 4, -- 6,
+        overall_tiling_vertical_padding = 4, -- 6,
+        overall_tiling_vertical_size = (28 - 8), -- 12),
+        overall_tiling_vertical_spacing = 6 -- 12
+    },
+    vertical_flow_style = {type = "vertical_flow_style", vertical_spacing = 0}
+}
+
+styles.tte_junction_size_slider = {
+    type = "slider_style",
+    horizontally_stretchable = "on",
+    left_margin = 12,
+    right_margin = 12,
+    top_margin = 7
+}
+
+styles.tte_junction_size_textfield = {
+    type = "textbox_style",
+    top_margin = -2,
+    horizontal_align = "center",
+    width = 80
+}
